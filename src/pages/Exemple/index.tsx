59a7195bfbef39534992030a2c59761fb4fa70e0
Arllan Pablo <arllanpablo@gmail.com>
2022-03-20 00:20:49 -0300

    refactor: ajuste de componentes de telas

--- src/pages/Exemple/index.tsx
+++ src/pages/Exemple/index.tsx
@@ -1,50 +1,24 @@
 import React, {useState} from 'react';
 import {Switch} from 'react-native';
 import {Menu} from '../../components/Menu';
+import {CardControl} from '../../components/CardControl';
 
-import {
-  Container,
-  Text,
-  Button,
-  CardText,
-  TextCard,
-  Input,
-  Title,
-  Row,
-  Line,
-  TextToogle,
-} from './styles';
+import {Container, Content} from './styles';
 
 export function Exemple() {
   const [toggleSwitch, setToggleSwitch] = useState(false);
   return (
     <Container>
-      <Row>
-        <Menu navigationRow="exemple" labelmenu="CONTROLE DE PORTAS" />
-        <CardText>
-          <Title>Descrição</Title>
-          <TextCard>
-            Para realizar o experimento e preciso verificar se a placa está
-            conectada na mesma rede que o device está conectado atualmente.
-          </TextCard>
-        </CardText>
-      </Row>
-
-      <Line>
-        <TextToogle>PORTA DIGITAL 01</TextToogle>
-        <Switch
-          trackColor={{false: '#767577', true: '#81b0ff'}}
-          thumbColor={toggleSwitch ? '#1E90FF' : '#f4f3f4'}
+      <Menu navigationRow="exemple" labelmenu="CONTROLE DE PORTAS" />
+      <Content>
+        <CardControl
+          pin="01"
           value={toggleSwitch}
           onValueChange={val => {
             setToggleSwitch(val);
           }}
         />
-      </Line>
-
-      <Button>
-        <Text>TESTAR</Text>
-      </Button>
+      </Content>
     </Container>
   );
 }
--- src/pages/Exemple/styles.ts
+++ src/pages/Exemple/styles.ts
@@ -7,108 +7,13 @@ export const Container = styled.View`
   flex: 1;
   background-color: ${({theme}) => theme.colors.background};
   flex-direction: column;
-  justify-content: space-between;
+  justify-content: flex-start;
   align-items: center;
 `;
 
-export const Text = styled.Text`
-  color: ${({theme}) => theme.colors.shape};
-  font-size: ${normalizePx(18)};
-  font-weight: bold;
-`;
-
-export const TextToogle = styled.Text`
-  color: ${({theme}) => theme.colors.black};
-  font-size: ${normalizePx(16)};
-  font-weight: 500;
-`;
-
-export const Button = styled.TouchableOpacity`
-  width: 90%;
-  height: ${normalizePx(50)};
-  border-radius: ${normalizePx(20)};
-  background-color: ${({theme}) => theme.colors.attention};
-  align-items: center;
-  justify-content: center;
-  margin-bottom: ${normalizePx(15)};
-`;
-
-export const TextCard = styled.Text`
-  color: ${({theme}) => theme.colors.backgroundPrimary};
-  font-size: ${normalizePx(18)};
-  font-weight: normal;
-  text-align: justify;
-`;
-
-export const Title = styled.Text`
-  color: ${({theme}) => theme.colors.black};
-  font-size: ${normalizePx(18)};
-  font-weight: bold;
-  margin-bottom: 5px;
-  text-align: justify;
-`;
-
-export const CardText = styled.View`
-  width: 90%;
-  /* min-height: ${normalizePx(250)}; */
-  margin-top: 30px;
-  background-color: ${({theme}) => theme.colors.shape};
-  border-radius: ${normalizePx(10)};
-  padding: 10px 10px;
-  border-color: ${({theme}) => theme.colors.shape};
-  /* box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.2); */
-
-  /* border: 1px solid ${({theme}) => theme.colors.backgroundPrimary}; */
-  /* border-radius: 16px; */
-`;
-
-export const Row = styled.View`
+export const Content = styled.View`
   width: 100%;
+  margin-top: ${normalizePx(25)};
+  justify-content: flex-start;
   align-items: center;
-  flex-direction: column;
 `;
-
-export const Line = styled.View`
-  width: 100%;
-  height: ${normalizePx(50)};
-  background-color: ${({theme}) => theme.colors.shape};
-  align-items: center;
-  flex-direction: row;
-`;
-
-export const Input = styled.TextInput`
-  width: 90%;
-  height: ${normalizePx(50)};
-  border-color: #000;
-  border-width: 1px;
-`;
-
-export const Card = styled.View`
-  width: 90%;
-  height: ${normalizePx(140)};
-  align-items: center;
-  justify-content: space-between;
-  border-width: 1px;
-  border-color: ${({theme}) => theme.colors.success};
-`;
-
-export const LineCard = styled.View`
-  flex-direction: row;
-  width: 100%;
-  margin-top: 2px;
-  padding-left: 2px;
-`;
-
-export const TextArea = styled.TextInput`
-  width: 90%;
-  min-height: ${normalizePx(50)};
-  border-width: 2px;
-  border-radius: ${normalizePx(10)};
-  border-color: ${({theme}) => theme.colors.attention};
-`;
-
-export const Icons = styled(Icon).attrs({
-  name: 'add',
-  size: 30,
-  color: theme.colors.background,
-})``;
