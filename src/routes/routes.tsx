ae5fe29a98a69ae6351df0fcb53c8ecb07e5070d
Arllan Pablo <arllanpablo@gmail.com>
2022-03-21 21:37:48 -0300

    refactor: ajuste de rotas para tela de intro

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -5,16 +5,18 @@ import {screenNames} from './screenNames';
 
 import {Home} from '../pages/Home';
 import {Exemple} from '../pages/Exemple';
+import {Intro} from '../pages/Intro';
 
 export function Routes() {
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Home"
+        initialRouteName="Intro"
         screenOptions={{headerShown: false}}>
         <Stack.Screen name={screenNames.exemplo} component={Exemple} />
         <Stack.Screen name={screenNames.home} component={Home} />
+        <Stack.Screen name={screenNames.Intro} component={Intro} />
       </Stack.Navigator>
     </NavigationContainer>
   );
